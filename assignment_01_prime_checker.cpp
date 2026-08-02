f75143d2b35c7046eca26b8c66adf2d8a1e29461
ofosuhenehenry8-ai <ofosuhenehenry8@gmail.com>
2026-08-02 22:54:18 +0000

    Update assignment_01_prime_checker.cpp

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -36,4 +36,27 @@
 
 #include <iostream>
 using namespace std;
+bool isPrime(int n){
+    if (n < 2) {
+        return false;
+    }
+    for (int i =2; i * i <=n;i++){
+        if (n%i==0) {
+            return false;
+        }
+    }
+    return true;
+}
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    if (isPrime(number)){
+        cout << number << " is a prime number.";
+    }
+    else{
+       cout << number << " is not a prime number.";
+    }
+
+}
 
