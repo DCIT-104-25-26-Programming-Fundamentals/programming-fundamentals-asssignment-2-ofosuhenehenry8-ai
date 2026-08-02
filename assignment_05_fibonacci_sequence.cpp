9ad5d8cba13f934275d2ecd9ff6629c404bf0a3c
ofosuhenehenry8-ai <ofosuhenehenry8@gmail.com>
2026-08-02 22:58:19 +0000

    Update assignment_05_fibonacci_sequence.cpp

--- assignment_05_fibonacci_sequence.cpp
+++ assignment_05_fibonacci_sequence.cpp
@@ -51,3 +51,114 @@
 #include <iostream>
 using namespace std;
 
+void printFibonacci();
+void checkFibonacci();
+
+int main()
+{
+    int choice;
+
+    do
+    {
+        cout << "\n===== FIBONACCI SEQUENCE GENERATOR =====\n";
+        cout << "1. Print the first N Fibonacci terms\n";
+        cout << "2. Check if a number is a Fibonacci number\n";
+        cout << "3. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            printFibonacci();
+            break;
+
+        case 2:
+            checkFibonacci();
+            break;
+
+        case 3:
+            cout << "Goodbye!\n";
+            break;
+
+        default:
+            cout << "Invalid choice. Try again.\n";
+        }
+
+    } while (choice != 3);
+
+    return 0;
+}
+
+
+void printFibonacci()
+{
+    int n;
+
+    cout << "\nHow many terms? ";
+    cin >> n;
+
+    if (n <= 0)
+    {
+        cout << "Error: N must be a positive integer.\n";
+        return;
+    }
+
+    int first = 0, second = 1, next;
+
+    cout << "Fibonacci sequence: ";
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == 1)
+        {
+            cout << first << " ";
+        }
+        else if (i == 2)
+        {
+            cout << second << " ";
+        }
+        else
+        {
+            next = first + second;
+            cout << next << " ";
+            first = second;
+            second = next;
+        }
+    }
+
+    cout << endl;
+}
+
+
+void checkFibonacci()
+{
+    int number;
+
+    cout << "\nEnter a number to check: ";
+    cin >> number;
+
+    if (number < 0)
+    {
+        cout << number << " is NOT a Fibonacci number.\n";
+        return;
+    }
+
+    int first = 0, second = 1, next = 0;
+
+    while (next < number)
+    {
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    if (number == 0 || number == 1 || next == number)
+    {
+        cout << number << " is a Fibonacci number.\n";
+    }
+    else
+    {
+        cout << number << " is NOT a Fibonacci number.\n";
+    }
+}
